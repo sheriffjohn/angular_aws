6950867b5cb3b68db13592cbabf8e05f179ddb0e
Felicia Ringblom <felicia.ringblom@gmail.com>
2018-04-06 11:19:47 +0200

    remove css files

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dropdown',
-  templateUrl: './dropdown.component.html',
-  styleUrls: ['./dropdown.component.css']
+  templateUrl: './dropdown.component.html'
 })
 export class DropdownComponent implements OnInit {
 
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  templateUrl: './search.component.html'
 })
 export class SearchComponent implements OnInit {
 
